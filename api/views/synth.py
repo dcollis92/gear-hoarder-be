1f2551bf08fb06619ddfe64f710b338f8787b278
Tim Crisp <timothycrisp@gmail.com>
2022-04-21 07:57:16 -0500

    create synth route

---
+++ api/views/synth.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, jsonify, request
+from api.middleware import login_required, read_token
+
+from api.models.db import db
+from api.models.synth import Synth
+
+synths = Blueprint('synths', 'synths')
+
+@synth.route('/', methods=["POST"])
+@login_required
+def create():
+  data = request.get_json()
+  profile = read_token(request)
+  data["profile_id"] = profile["id"]
+
+  synth = Synth(**data)
+  db.session.add(synth)
+  db.session.commit()
+  return jsonify(synth.serialize()), 201
